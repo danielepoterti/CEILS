0d80e3555a80234d39412ca0ecf778c5f6385f67
Daniele Potertì <daniele.poterti@gmail.com>
2023-06-29 15:42:26 +0200

    build_causal_skeleton fixed

--- core/causal/build_causal_skeleton.py
+++ core/causal/build_causal_skeleton.py
@@ -2,102 +2,72 @@ from causallearn.search.ConstraintBased.PC import pc
 from causallearn.search.ConstraintBased.FCI import fci
 from causallearn.search.ScoreBased.GES import ges
 from causallearn.search.FCMBased import lingam
-from causallearn.utils.GraphUtils import GraphUtils
 import networkx as nx
-import numpy as np
+import sys
+import os
 
 def build_causal_skeleton(df):
-
+    
     pc_G = build_PC(df)
-
     fci_G = build_FCI(df)
     ges_G = build_GES(df)
     lingam_G = build_LiNGAM(df)
 
-    
+    union_G = nx.compose(pc_G, fci_G)
+    union_G = nx.compose(union_G, ges_G)
+    union_G = nx.compose(union_G, lingam_G)
 
     
 
-    union_G = nx.union(pc_G, fci_G,)
-
-    union_G = nx.union(union_G, ges_G)
-
-    union_G = nx.union(union_G, lingam_G)
-    
-
-    return union_G.to_undirected()
+    return union_G
 
 def build_PC(df):
-
-    # Convert pandas DataFrame to numpy array
     data_array = df.values
-
-    # Get the graph
-    CG = pc(data_array)
-
-    CG.draw_pydot_graph(labels = df.columns.tolist())
-
+    CG = pc(data_array, node_names=df.columns.tolist(), show_progress=False)
     G = CG.G
-
     undirected_graph = nx.Graph()
-
     for edge in G.get_graph_edges():
-
-        u = edge.get_node1() 
-        v = edge.get_node2()
-
-        # Step 4: Add undirected edge (u, v) and (v, u)
+        u = edge.get_node1().get_name()
+        v = edge.get_node2().get_name()
         undirected_graph.add_edge(u, v)
-
-    # Convert GeneralGraph to NetworkX graph
     return undirected_graph
 
 def build_FCI(df):
-
-    # Convert pandas DataFrame to numpy array
     data_array = df.values
-
-    # Get the graph
-    G, _ = fci(data_array)
-
+    original_stdout = sys.stdout
+    sys.stdout = sys.stderr = open(os.devnull, 'w')
+    G, _ = fci(data_array, verbose = False, show_progress = False)
+    sys.stdout = original_stdout
     undirected_graph = nx.Graph()
-
     for edge in G.get_graph_edges():
-
-        u = edge.get_node1() 
-        v = edge.get_node2()
-
-        # Step 4: Add undirected edge (u, v) and (v, u)
+        u = edge.get_node1().get_name()
+        v = edge.get_node2().get_name()
         undirected_graph.add_edge(u, v)
-
-    # Convert GeneralGraph to NetworkX graph
+    mapping = {"X{}".format(i+1): name for i, name in enumerate(df.columns.tolist())}
+    undirected_graph = nx.relabel_nodes(undirected_graph, mapping)
     return undirected_graph
 
 def build_GES(df):
-
     data_array = df.values
+    original_stdout = sys.stdout
+    sys.stdout = sys.stderr = open(os.devnull, 'w')
     Record = ges(data_array)
-
+    sys.stdout = original_stdout
     G = Record['G']
-
     undirected_graph = nx.Graph()
-
-    
-
     for edge in G.get_graph_edges():
-        u = edge.get_node1() 
-        v = edge.get_node2()
-
-        # Step 4: Add undirected edge (u, v) and (v, u)
+        u = edge.get_node1().get_name()
+        v = edge.get_node2().get_name()
         undirected_graph.add_edge(u, v)
-
-    # Convert GeneralGraph to NetworkX graph
+    mapping = {"X{}".format(i+1): name for i, name in enumerate(df.columns.tolist())}
+    undirected_graph = nx.relabel_nodes(undirected_graph, mapping)
     return undirected_graph
 
 def build_LiNGAM(df):
     data_array = df.values
     model = lingam.ICALiNGAM(42, 700)
     model.fit(data_array)
-
-    # Convert numpy matrix to NetworkX graph
-    return nx.from_numpy_matrix(model.adjacency_matrix_)
+    undirected_graph = nx.convert_matrix.from_numpy_array(model.adjacency_matrix_)
+    mapping = {i: name for i, name in enumerate(df.columns.tolist())}
+    undirected_graph = nx.relabel_nodes(undirected_graph, mapping)
+    return undirected_graph
